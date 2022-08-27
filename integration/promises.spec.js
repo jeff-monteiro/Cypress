e3c64cbdb16ebadd396cb9d045e14f51ec50bab8
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-27 12:04:19 -0300

    promisesOnCypress

--- integration/promises.spec.js
+++ integration/promises.spec.js
@@ -2,15 +2,24 @@ it('no tests yet', () => {
 
 })
 
-const getSomething = (callback) => {
-    setTimeout(() => {
-        callback('12')
-    }, 1000)
+// const getSomething = (callback) => {
+//     setTimeout(() => {
+//         callback('12')
+//     }, 1000)
+// }
+
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(13)
+        }, 1000)
+    })
+    
 }
 
 const system = () => {
     console.log('init')
-    getSomething(some => console.log(`Something is ${some}`))
+    getSomething().then(some => console.log(`Something is ${some}`))
     console.log('end')
 }
 system()
