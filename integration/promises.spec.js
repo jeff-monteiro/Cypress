b4db222785fd74948dd1a45f760041f631e0838a
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-27 12:24:21 -0300

    promisesOnCypress

--- integration/promises.spec.js
+++ integration/promises.spec.js
@@ -2,11 +2,14 @@ it('no tests yet', () => {
 
 })
 
+//Using callback function
+
 // const getSomething = (callback) => {
 //     setTimeout(() => {
 //         callback('12')
 //     }, 1000)
 // }
+
 
 const getSomething = () => {
     return new Promise((resolve, reject) => {
@@ -23,4 +26,17 @@ const system = () => {
     console.log('end')
 }
 system()
+
+// Using async/await function but on cypress is not a good practice
+
+// const system = async () => {
+//     console.log('init')
+//     const some = await getSomething()
+//     console.log(`Something is ${some}`)
+//     console.log('end')
+// }
+
+// system()
+
+
 
