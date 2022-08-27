4cb05a9341cf6a4a005c7d4f89f96c9c745faac4
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-27 11:32:47 -0300

    callbacksOnCypress

--- integration/arrow.spec.js
+++ integration/arrow.spec.js
@@ -1,7 +1,31 @@
 it('nada agora', function() { })
 
-function soma(a, b){
-    return a + b
-}
+// function soma(a, b){
+//     return a + b
+// }
 
-console.log(soma(2, 4))
+// const soma = function (a, b){
+//     return a + b
+// }
+
+// const soma = (a, b) => {
+//     return a + b
+// }
+
+// console.log(soma(2, 4))
+
+const soma = () => 5 + 5
+
+console.log(soma())
+
+it('a function test...', function () {
+    console.log('Function', this)
+})
+
+it('a arrow test...', () => {
+    console.log('Arrow', this)
+})
+
+
+
+
