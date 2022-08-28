d0ff59f11c7af96f4c38d87c3453bb979c08fb47
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-28 01:43:56 -0300

    functions

--- e2e/basic.cy.js
+++ e2e/basic.cy.js
@@ -1,12 +1,24 @@
 describe('Cypress basic access', () => {
-    it('Should visit a page and make assert on title', () => {
+    it.only('Should visit a page and make assert on title', () => {
         cy.visit('http://wcaquino.me/cypress/componentes.html')
 
-        cy.title().should('be.equal', 'Campo de Treinamento')
-        cy.title().should('contains', 'Campo')
+        //cy.title().should('be.equal', 'Campo de Treinamento')
+        //cy.title().should('contains', 'Campo').debug()
+        //cy.pause()
 
         cy.title()
             .should('to.be.equal', 'Campo de Treinamento')
-            .should('contains', 'Treinamento')
+            .and('contains', 'Treinamento')
+
+        // TODO imprimir o log no console
+        // TODO escrever o log em um campo de teste
+    })
+
+    it('Should find and interact with element', () => {
+        cy.visit('http://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
     })
 })
