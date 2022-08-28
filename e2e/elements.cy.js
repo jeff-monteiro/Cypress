2366379b38c3ddd7931322a3ee0abd8c8e796198
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-08-28 17:00:25 -0300

    textFields

--- e2e/elements.cy.js
+++ e2e/elements.cy.js
@@ -18,4 +18,24 @@ describe('Work with basic elements', () => {
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
+
+    it.only('TextsFields', () => {
+        cy.get('#formNome')
+            .type('Jefferson Monteiro')
+            .should('have.value', 'Jefferson Monteiro')
+        
+        cy.get('#elementosForm\\:sugestoes').type('textos e mais textos')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('????')
+        
+        cy.get('#formSobrenome')
+            .type('Test12345{backspace}{backspace}')
+            .should('have.value', 'Test123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay:100})
+            .should('have.value', 'acerto')
+    })
 })
