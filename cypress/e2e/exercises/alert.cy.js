352cae478add0c7564b1edad9681a2425ee5e445
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-10-02 00:39:14 -0300

    setupNewVersion

---
+++ cypress/e2e/exercises/alert.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Working with alerts',  () => {
+    before(() => {
+        cy.visit('http://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Interacting with the alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+})
